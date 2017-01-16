b8bf7fa9a00c63c87e014c258a2282497d0897ec
erez <erez1004@gmail.com>
2017-01-17 01:19:45 +0200

    find-const,remove-dup

--- final.scm
+++ final.scm
@@ -1,6 +1,5 @@
 (load "compiler.scm")
 
-
 (define string->exprlist 
   (lambda (string)
     (if (or (null? string) (equal? string "")) '() 
@@ -9,22 +8,42 @@
 	      `(,e ,@(string->exprlist (list->string s))))
 	    (lambda (w) `(failed with report: ,@w))))
 	    ))
-(define parse_expr
-        (lambda (expr_list)
-            (annotate-tc(pe->lex-pe(box-set(remove-applic-lambda-nil(eliminate-nested-defines(parse expr_list))))))))
-            
+
+(define parse-expr
+        (lambda (expr-list)
+            (annotate-tc(pe->lex-pe(box-set(remove-applic-lambda-nil(eliminate-nested-defines(parse expr-list))))))))
+
+(define find-const 
+      (lambda (ast)
+          (letrec ((foo 
+                      (lambda(ast bag)
+                          (cond ((or (null? ast) (not(list? ast))) '())
+                                ((and (list? ast) (equal? (car ast) 'const)) (append bag (cdr ast)))
+                                (else (fold-left (lambda(bag expr)(append bag (foo expr '()))) bag ast))
+                            )
+                      )))
+            (foo ast '()))
+            ))    
+
+(define remove-dup
+      (lambda (lst)
+          (if (null? lst) '()
+              (cons (car lst) (remove-dup (filter (lambda (x) (not (equal? x (car lst)))) 
+                                            (cdr lst)))))))
+(define topological-sort
+      (lambda(const-list)
+          const-list))
+
 (define code-gen 
     (lambda (pe) 
-    ;string that contains lines of assembly instructions in the CISC architecture, such that when the execution of these instructions is complete,the value of e should be in the result register R0.
     '(assembly lines)))
     
 (define compile-scheme-file 
     (lambda (fileName assemblyTarget) 
             (let* ((file (file->string fileName))
-                   (exprList (string->exprlist file))
-                   (parsed_exprs (map parse_expr exprList))
+                   (expr-list (string->exprlist file))
+                   (parsed-exprs (map parse-expr expr-list))
+                   (const-table (topological-sort(remove-dup(find-const (car parsed-exprs)))))
                    )
-            parsed_exprs)
-            
-            
+            const-table)    
             ))
